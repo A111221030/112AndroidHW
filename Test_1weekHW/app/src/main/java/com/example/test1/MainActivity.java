8d8b4cc008a8b31ad5507c6886a03aaa4530b78b
A111221030 <A111221030@mail.shu.edu.tw>
2024-04-11 10:03:08 +0800

    2024-0411

---
+++ Test_1weekHW/app/src/main/java/com/example/test1/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.test1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void button_Click(View view){
+        EditText edtName = (EditText) findViewById(R.id.editName);
+        EditText txtPassWord =(EditText) findViewById(R.id.TxtPassword);
+        TextView txvShow  =(TextView) findViewById(R.id.textShow);
+
+        String name = edtName.getText().toString();
+        String password = txtPassWord.getText().toString();
+        if(name.equals("A111221030") && password.equals("OO171459") )
+        {
+            txvShow.setText("密碼輸入正確!\n你的學號是:" + name);
+        }
+        else
+        {
+            txvShow.setText("1輸入錯誤!\n帳號:A111221030\n密碼:OO171459" );
+        }
+
+    }
+}
